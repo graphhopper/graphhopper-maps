677a6760d9909991850fd8a80a422c041a622e71
Janekdererste <laudan@tu-berlin.de>
2021-02-15 21:52:06 +0100

    Adjust unit tests to changed signature of queryUrl functions

--- test/QueryUrl.test.ts
+++ test/QueryUrl.test.ts
@@ -1,40 +1,40 @@
 import {createUrl, parseUrl} from "../src/QueryUrl";
-import {ghKey} from "../src/routing/Api";
+import {QueryPoint} from "../src/stores/QueryStore";
 
 describe("parseUrl", () => {
 
     it("should parse points from a url", () => {
 
-        const point1 = [7.275303695325306, 50.67724646887518]
-        const point2 = [10.81515858598078, 50.28050431501495]
+        const point1 = [7.275303695325306, 50.67724646887518] as [number, number]
+        const point2 = [10.81515858598078, 50.28050431501495] as [number, number]
         const url = `http://localhost:3000/?point=${point1.join(',')}&point=${point2.join(',')}`
 
-        const request = parseUrl(url)
+        const points = parseUrl(url)
 
-        expect(request.points.length).toEqual(2)
-        expect(request.points[0]).toEqual(point1)
-        expect(request.points[1]).toEqual(point2)
+        expect(points.length).toEqual(2)
+        expect(points[0]).toEqual(coordinateToQueryPoint(point1, 0))
+        expect(points[1]).toEqual(coordinateToQueryPoint(point2, 1))
     })
 
     it("should create an empty request when no points are supplied", () => {
         const url = `http://localhost:3000/?`
 
-        const request = parseUrl(url)
+        const points = parseUrl(url)
 
-        expect(request.points.length).toEqual(0)
+        expect(points.length).toEqual(0)
     })
 
     it("should ignore other params than points", () => {
 
-        const point1 = [7.275303695325306, 50.67724646887518]
-        const point2 = [10.81515858598078, 50.28050431501495]
+        const point1 = [7.275303695325306, 50.67724646887518] as [number, number]
+        const point2 = [10.81515858598078, 50.28050431501495] as [number, number]
         const url = `http://localhost:3000/?point=${point1.join(',')}&point=${point2.join(',')}&some-param=some-value`
 
-        const request = parseUrl(url)
+        const points = parseUrl(url)
 
-        expect(request.points.length).toEqual(2)
-        expect(request.points[0]).toEqual(point1)
-        expect(request.points[1]).toEqual(point2)
+        expect(points.length).toEqual(2)
+        expect(points[0]).toEqual(coordinateToQueryPoint(point1, 0))
+        expect(points[1]).toEqual(coordinateToQueryPoint(point2, 1))
     })
 
     it("should raise an error if a point is not in the expected format", () => {
@@ -55,8 +55,17 @@ describe("createUrl", () => {
         expectedUrl.searchParams.append("point", point1.join(','))
         expectedUrl.searchParams.append("point", point2.join(','))
 
-        const result = createUrl(expectedUrl.origin, {points: [point1, point2], key: ghKey})
+        const result = createUrl(expectedUrl.origin, [coordinateToQueryPoint(point1, 1), coordinateToQueryPoint(point2, 2)])
 
         expect(result).toEqual(expectedUrl)
     })
 })
+
+function coordinateToQueryPoint(coordinate: [number, number], id: number): QueryPoint {
+    return {
+        isInitialized: true,
+        point: {lng: coordinate[0], lat: coordinate[1]},
+        queryText: '',
+        id: id
+    }
+}
