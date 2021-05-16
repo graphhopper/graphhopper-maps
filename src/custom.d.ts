03457beba3608a9f5d0d414902d647789d070a68
easbar <easbar.mail@posteo.net>
2021-05-16 11:38:28 +0200

    Remove module

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,6 +1,5 @@
 declare module '*.css'
 declare module '*.svg'
 declare module '*.png'
-declare module 'leaflet.heightgraph/example/MapboxHeightGraph'
 declare module 'leaflet.heightgraph/src/heightgraph'
 
