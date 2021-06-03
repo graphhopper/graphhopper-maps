99f700e01135f99858aa294af214cb06a9c5d244
Janek Laudan <j.laudan@mailbox.org>
2021-06-03 12:29:32 +0200

    #32 autocomplete keyboard (#88)
    * Navigate geocoding results with arrow keys
    
    * Leave full screen on Esc
    
    * Fix broken border around geocoding autocomplete
    
    * Add debounce to geocoding
    
    * remove logging
    
    * Clean up AddressInput.tsx

--- src/sidebar/search/AddressInput.tsx
+++ src/sidebar/search/AddressInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { QueryPoint } from '@/stores/QueryStore'
 import { GeocodingHit } from '@/api/graphhopper'
 import GeocodingResult from '@/sidebar/search/GeocodingResult'
@@ -15,43 +15,75 @@ export interface AddressInputProps {
 }
 
 export default function AddressInput(props: AddressInputProps) {
-    const searchInput = useRef<HTMLInputElement>(null)
-
-    // holds the query text making this a controlled component
+    // controlled component pattern with initial value set from props
     const [text, setText] = useState(props.point.queryText)
-    // puts the query text from props into the state of this component
     useEffect(() => setText(props.point.queryText), [props.point.queryText])
 
+    // container for geocoding results which get set by the geocoder class and set to empty if the undelying query point gets changed from outside
     const [geocodingResults, setGeocodingResults] = useState<GeocodingHit[]>([])
     const [geocoder] = useState(new Geocoder(hits => setGeocodingResults(hits)))
-    useEffect(() => setGeocodingResults([]), [props.point])
+    useEffect(() => {
+        setGeocodingResults([])
+        //setFullscreen(false)
+    }, [props.point])
 
+    // highlighted result of geocoding results. Keep track which index is highlighted and change things on ArrowUp and Down
+    // on Enter select highlighted result or the 0th if nothing is highlighted
+    const [highlightedResult, setHighlightedResult] = useState<number>(-1)
+    useEffect(() => setHighlightedResult(-1), [geocodingResults])
+    const searchInput = useRef<HTMLInputElement>(null)
+    const onKeypress = useCallback(
+        (event: React.KeyboardEvent<HTMLInputElement>) => {
+            if (event.key === 'Escape') {
+                searchInput.current!.blur()
+                return
+            }
+            if (geocodingResults.length === 0) return
+
+            switch (event.key) {
+                case 'ArrowUp':
+                    setHighlightedResult(i => calculateHighlightedIndex(geocodingResults.length, i, -1))
+                    break
+                case 'ArrowDown':
+                    setHighlightedResult(i => calculateHighlightedIndex(geocodingResults.length, i, 1))
+                    break
+                case 'Enter':
+                    // by default use the first result, otherwise the highlighted one
+                    const index = highlightedResult >= 0 ? highlightedResult : 0
+                    props.onAddressSelected(geocodingResults[index])
+                    searchInput.current!.blur()
+                    break
+            }
+        },
+        [geocodingResults, highlightedResult]
+    )
+
+    // toggle fullscreen display on small screens
     const [fullscreen, setFullscreen] = useState(false)
-
     const containerClass = fullscreen ? styles.container + ' ' + styles.fullscreen : styles.container
+
     return (
         <div className={containerClass}>
             <div className={styles.inputContainer}>
                 <input
                     className={styles.input}
-                    type='text'
+                    type="text"
                     ref={searchInput}
                     onChange={e => {
                         setText(e.target.value)
                         geocoder.request(e.target.value)
                         props.onChange(e.target.value)
                     }}
+                    onKeyDown={onKeypress}
                     onFocus={() => setFullscreen(true)}
+                    onBlur={() => {
+                        setFullscreen(false)
+                        setGeocodingResults([])
+                    }}
                     value={text}
                     placeholder={'Search location or right click on the map'}
                 />
-                <button
-                    className={styles.btnClose}
-                    onClick={() => {
-                        console.log(searchInput.current)
-                        setFullscreen(false)
-                    }}
-                >
+                <button className={styles.btnClose} onClick={() => setFullscreen(false)}>
                     Close
                 </button>
             </div>
@@ -60,9 +92,10 @@ export default function AddressInput(props: AddressInputProps) {
                 <div className={styles.popup}>
                     <GeocodingResult
                         hits={geocodingResults}
+                        highlightedHit={geocodingResults[highlightedResult]}
                         onSelectHit={hit => {
                             props.onAddressSelected(hit)
-                            setFullscreen(false)
+                            searchInput.current!.blur()
                         }}
                     />
                 </div>
@@ -71,32 +104,44 @@ export default function AddressInput(props: AddressInputProps) {
     )
 }
 
+function calculateHighlightedIndex(length: number, currentIndex: number, incrementBy: number) {
+    const nextIndex = currentIndex + incrementBy
+    if (nextIndex >= length) return 0
+    if (nextIndex < 0) return length - 1
+    return nextIndex
+}
+
 /**
- * Tried for hours to make this work with a hook but failed. Now doing it the way I know...
+ * This could definitely be achieved with an effect. But after trying for a while I saved some money and wrote it the
+ * Way I know. If we hire an 10+ react developer, this should be changed.
  */
 class Geocoder {
     private requestId = 0
-    private api = new ApiImpl()
-    private onSuccess: (hits: GeocodingHit[]) => void
+    private readonly timeout = new Timout(500)
+    private readonly api = new ApiImpl()
+    private readonly onSuccess: (hits: GeocodingHit[]) => void
 
     constructor(onSuccess: (hits: GeocodingHit[]) => void) {
         this.onSuccess = onSuccess
     }
 
     request(query: string) {
-        const currentId = this.getNextId()
+        this.requestAsync(query).then(() => {})
+    }
 
+    async requestAsync(query: string) {
+        const currentId = this.getNextId()
+        this.timeout.cancel()
         if (!query) return
 
-        this.api
-            .geocode(query)
-            .then(result => {
-                const hits = Geocoder.filterDuplicates(result.hits)
-                if (currentId === this.requestId) this.onSuccess(hits)
-            })
-            .catch(reason => {
-                throw Error('Could not get geocoding results because: ' + reason)
-            })
+        await this.timeout.wait()
+        try {
+            const result = await this.api.geocode(query)
+            const hits = Geocoder.filterDuplicates(result.hits)
+            if (currentId === this.requestId) this.onSuccess(hits)
+        } catch (reason) {
+            throw Error('Could not get geocoding results because: ' + reason)
+        }
     }
 
     private getNextId() {
@@ -104,7 +149,7 @@ class Geocoder {
         return this.requestId
     }
 
-    private static filterDuplicates = function(hits: GeocodingHit[]) {
+    private static filterDuplicates(hits: GeocodingHit[]) {
         const set: Set<string> = new Set()
         return hits.filter(hit => {
             if (!set.has(hit.osm_id)) {
@@ -113,5 +158,23 @@ class Geocoder {
             }
             return false
         })
+    }
+}
+
+class Timout {
+    private readonly delay: number
+    private handle: number = 0
+
+    constructor(delay: number) {
+        this.delay = delay
+    }
+
+    wait() {
+        return new Promise(resolve => {
+            this.handle = window.setTimeout(resolve, this.delay)
+        })
+    }
+    cancel() {
+        clearTimeout(this.handle)
     }
 }
