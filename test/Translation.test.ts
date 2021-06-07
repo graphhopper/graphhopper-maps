c4069df0eab2f2cfa2312ebfcf0ac139971a36b5
Peter <graphhopper@gmx.de>
2021-06-07 11:16:35 +0200

    refactoring is a bit weak in VSCode

--- test/Translation.test.ts
+++ test/Translation.test.ts
@@ -3,13 +3,13 @@ import { setTranslation } from '../src/translation/Translation'
 describe('translate', () => {
     it('should translate', () => {
         const tr = setTranslation('de', true)
-        expect(tr.tr('unknownKEY')).toEqual('unknownKEY')
-        expect(tr.tr('racingbike')).toEqual('Rennrad')
-        expect(tr.tr('route_info', ['27km', '18min'])).toEqual('27km werden 18min brauchen')
+        expect(tr.get('unknownKEY')).toEqual('unknownKEY')
+        expect(tr.get('racingbike')).toEqual('Rennrad')
+        expect(tr.get('route_info', ['27km', '18min'])).toEqual('27km werden 18min brauchen')
     })
 
     it('should fallback', () => {
         const tr = setTranslation('fi', true)
-        expect(tr.tr('drag_to_reorder')).toEqual('Drag to reorder')
+        expect(tr.get('drag_to_reorder')).toEqual('Drag to reorder')
     })
 })
