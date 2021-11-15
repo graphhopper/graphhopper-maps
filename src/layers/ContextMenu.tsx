cb3e74ec91cf0360417a0806919f1204ab315f2a
easbar <easbar.mail@posteo.net>
2021-11-15 21:10:31 +0100

    Make context menu work on ios using long touch handler

--- src/layers/ContextMenu.tsx
+++ src/layers/ContextMenu.tsx
@@ -23,17 +23,24 @@ export default function ({ map, queryPoints }: ContextMenuProps) {
         })
         setOverlay(overlay)
         map.addOverlay(overlay)
+
+        const onMenuEvent = (e : any) => {
+            e.preventDefault()
+            const coordinate = map.getEventCoordinate(e)
+            const lonLat = toLonLat(coordinate)
+            setMenuCoordinate({ lng: lonLat[0], lat: lonLat[1] })
+            overlay.setPosition(coordinate)
+        }
+        const longTouchHandler = new LongTouchHandler((e) => onMenuEvent(e))
+
         map.once('change:target', () => {
             // we cannot listen to right-click simply using map.on('contextmenu') and need to add the listener to
             // the map container instead
             // https://github.com/openlayers/openlayers/issues/12512#issuecomment-879403189
-            map.getTargetElement().addEventListener('contextmenu', e => {
-                e.preventDefault()
-                const coordinate = map.getEventCoordinate(e)
-                const lonLat = toLonLat(coordinate)
-                setMenuCoordinate({ lng: lonLat[0], lat: lonLat[1] })
-                overlay.setPosition(coordinate)
-            })
+            map.getTargetElement().addEventListener('contextmenu', e => onMenuEvent(e))
+            map.getTargetElement().addEventListener('touchstart', e => longTouchHandler.onTouchStart(e))
+            map.getTargetElement().addEventListener('touchmove', () => longTouchHandler.onTouchEnd())
+            map.getTargetElement().addEventListener('touchend', () => longTouchHandler.onTouchEnd())
         })
     }, [map])
     return (
@@ -50,4 +57,28 @@ export default function ({ map, queryPoints }: ContextMenuProps) {
             )}
         </div>
     )
+}
+
+class LongTouchHandler {
+    private readonly callback: (e : any) => void
+    private currentTimeout: number = 0
+    private currentEvent? : any
+
+    constructor(onLongTouch: (e : any) => void) {
+        this.callback = onLongTouch
+    }
+
+    onTouchStart(e : any) {
+        this.currentEvent = e
+        this.currentTimeout = window.setTimeout(() => {
+            console.log('long touch')
+            if (this.currentEvent) this.callback(this.currentEvent)
+        }, 500)
+    }
+
+    onTouchEnd() {
+        console.log('touch end')
+        window.clearTimeout(this.currentTimeout)
+        this.currentEvent = undefined
+    }
 }
