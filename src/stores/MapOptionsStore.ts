280229c778a05a79b847a59cba6767ee6445c2eb
Peter <graphhopper@gmx.de>
2022-01-31 13:44:39 +0100

    detect retina to avoid requesting details where unnecessary, add retina for omniscale too

--- src/stores/MapOptionsStore.ts
+++ src/stores/MapOptionsStore.ts
@@ -1,6 +1,6 @@
 import Store from '@/stores/Store'
-import { Action } from '@/stores/Dispatcher'
-import { MapIsLoaded, SelectMapStyle } from '@/actions/Actions'
+import {Action} from '@/stores/Dispatcher'
+import {MapIsLoaded, SelectMapStyle} from '@/actions/Actions'
 import config from 'config'
 
 const osApiKey = config.keys.omniscale
@@ -36,6 +36,12 @@ export interface VectorStyle extends StyleOption {
     url: string
 }
 
+const mediaQuery = '(-webkit-min-device-pixel-ratio: 1.5),(min--moz-device-pixel-ratio: 1.5),(-o-min-device-pixel-ratio: 3/2),(min-resolution: 1.5dppx)'
+const isRetina = window.devicePixelRatio > 1 || window.matchMedia && window.matchMedia(mediaQuery).matches
+// instead of an increased tilePixelRatio we could use tileSize: 512 if less request should be made and overzoom should be sharper
+const tilePixelRatio = isRetina ? 2 : 1
+const retina2x = isRetina ? '@2x' : '';
+
 const mapTiler: VectorStyle = {
     name: 'MapTiler',
     type: 'vector',
@@ -62,8 +68,9 @@ const osmOrg: RasterStyle = {
 const omniscale: RasterStyle = {
     name: 'Omniscale',
     type: 'raster',
-    url: ['https://maps.omniscale.net/v2/' + osApiKey + '/style.default/{z}/{x}/{y}.png'],
+    url: ['https://maps.omniscale.net/v2/' + osApiKey + '/style.default/{z}/{x}/{y}.png' + (isRetina ? "?hq=true" : "")],
     attribution: osmAttribution + ', &copy; <a href="https://maps.omniscale.com/" target="_blank">Omniscale</a>',
+    tilePixelRatio: tilePixelRatio
 }
 const esriSatellite: RasterStyle = {
     name: 'Esri Satellite',
@@ -78,68 +85,68 @@ const tfTransport: RasterStyle = {
     name: 'TF Transport',
     type: 'raster',
     url: [
-        'https://a.tile.thunderforest.com/transport/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://b.tile.thunderforest.com/transport/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://c.tile.thunderforest.com/transport/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
+        'https://a.tile.thunderforest.com/transport/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://b.tile.thunderforest.com/transport/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://c.tile.thunderforest.com/transport/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
     ],
     attribution:
         osmAttribution +
         ', <a href="https://www.thunderforest.com/maps/transport/" target="_blank">Thunderforest Transport</a>',
-    tilePixelRatio: 2,
+    tilePixelRatio: tilePixelRatio,
 }
 const tfCycle: RasterStyle = {
     name: 'TF Cycle',
     type: 'raster',
     url: [
-        'https://a.tile.thunderforest.com/cycle/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://b.tile.thunderforest.com/cycle/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://c.tile.thunderforest.com/cycle/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
+        'https://a.tile.thunderforest.com/cycle/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://b.tile.thunderforest.com/cycle/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://c.tile.thunderforest.com/cycle/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
     ],
     attribution:
         osmAttribution +
         ', <a href="https://www.thunderforest.com/maps/opencyclemap/" target="_blank">Thunderforest Cycle</a>',
-    tilePixelRatio: 2,
+    tilePixelRatio: tilePixelRatio,
 }
 const tfOutdoors: RasterStyle = {
     name: 'TF Outdoors',
     type: 'raster',
     url: [
-        'https://a.tile.thunderforest.com/outdoors/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://b.tile.thunderforest.com/outdoors/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://c.tile.thunderforest.com/outdoors/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
+        'https://a.tile.thunderforest.com/outdoors/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://b.tile.thunderforest.com/outdoors/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://c.tile.thunderforest.com/outdoors/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
     ],
     attribution:
         osmAttribution +
         ', <a href="https://www.thunderforest.com/maps/outdoors/" target="_blank">Thunderforest Outdoors</a>',
-    tilePixelRatio: 2,
+    tilePixelRatio: tilePixelRatio,
 }
 const tfAtlas: RasterStyle = {
     name: 'TF Atlas',
     type: 'raster',
     url: [
-        'https://a.tile.thunderforest.com/atlas/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://b.tile.thunderforest.com/atlas/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
-        'https://c.tile.thunderforest.com/atlas/{z}/{x}/{y}@2x.png?apikey=' + thunderforestApiKey,
+        'https://a.tile.thunderforest.com/atlas/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://b.tile.thunderforest.com/atlas/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
+        'https://c.tile.thunderforest.com/atlas/{z}/{x}/{y}' + retina2x + '.png?apikey=' + thunderforestApiKey,
     ],
     attribution:
         osmAttribution + ', <a href="https://thunderforest.com/maps/atlas/" target="_blank">Thunderforest Atlas</a>',
-    tilePixelRatio: 2,
+    tilePixelRatio: tilePixelRatio,
 }
 const kurviger: RasterStyle = {
     name: 'Kurviger Liberty',
     type: 'raster',
     url: [
-        'https://a-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}@2x.png?key=' + kurvigerApiKey,
-        'https://b-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}@2x.png?key=' + kurvigerApiKey,
-        'https://c-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}@2x.png?key=' + kurvigerApiKey,
-        'https://d-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}@2x.png?key=' + kurvigerApiKey,
-        'https://e-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}@2x.png?key=' + kurvigerApiKey,
+        'https://a-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}' + retina2x + '.png?key=' + kurvigerApiKey,
+        'https://b-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}' + retina2x + '.png?key=' + kurvigerApiKey,
+        'https://c-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}' + retina2x + '.png?key=' + kurvigerApiKey,
+        'https://d-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}' + retina2x + '.png?key=' + kurvigerApiKey,
+        'https://e-tiles.mapilion.com/raster/styles/kurviger-liberty/{z}/{x}/{y}' + retina2x + '.png?key=' + kurvigerApiKey,
     ],
     attribution:
         osmAttribution +
         ',&copy; <a href="https://kurviger.de/" target="_blank">Kurviger</a> &copy; <a href="https://mapilion.com/attribution" target="_blank">Mapilion</a> <a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a>',
     maxZoom: 22,
-    tilePixelRatio: 2,
+    tilePixelRatio: tilePixelRatio,
 }
 const mapillion: VectorStyle = {
     name: 'Mapilion',
