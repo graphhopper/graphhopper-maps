44c69d9a4e2e8c1da1374bc3318f41798daf8999
Peter <graphhopper@gmx.de>
2022-02-01 10:35:16 +0100

    remove misleading comment

--- src/stores/MapOptionsStore.ts
+++ src/stores/MapOptionsStore.ts
@@ -39,7 +39,6 @@ export interface VectorStyle extends StyleOption {
 const mediaQuery =
     '(-webkit-min-device-pixel-ratio: 1.5),(min--moz-device-pixel-ratio: 1.5),(-o-min-device-pixel-ratio: 3/2),(min-resolution: 1.5dppx)'
 const isRetina = window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(mediaQuery).matches)
-// instead of an increased tilePixelRatio we could use tileSize: 512 if less request should be made and overzoom should be sharper
 const tilePixelRatio = isRetina ? 2 : 1
 const retina2x = isRetina ? '@2x' : ''
 
