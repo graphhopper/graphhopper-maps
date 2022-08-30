b0b3d95f78bb254c898eb54af6f4a4d656e535e0
Peter <graphhopper@gmx.de>
2022-08-30 16:35:17 +0200

    no need for LongTouchHandler, #135

--- src/layers/ContextMenu.tsx
+++ src/layers/ContextMenu.tsx
@@ -33,16 +33,12 @@ export default function ContextMenu({ map, route, queryPoints }: ContextMenuProp
             setMenuCoordinate({ lng: lonLat[0], lat: lonLat[1] })
             overlay.setPosition(coordinate)
         }
-        const longTouchHandler = new LongTouchHandler(e => onMenuEvent(e))
 
         map.once('change:target', () => {
             // we cannot listen to right-click simply using map.on('contextmenu') and need to add the listener to
             // the map container instead
             // https://github.com/openlayers/openlayers/issues/12512#issuecomment-879403189
             map.getTargetElement().addEventListener('contextmenu', e => onMenuEvent(e))
-            map.getTargetElement().addEventListener('touchstart', e => longTouchHandler.onTouchStart(e))
-            map.getTargetElement().addEventListener('touchmove', () => longTouchHandler.onTouchEnd())
-            map.getTargetElement().addEventListener('touchend', () => longTouchHandler.onTouchEnd())
         })
     }, [map])
     return (
