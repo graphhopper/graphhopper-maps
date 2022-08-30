b094662c42110b933e523f3739d03ecfb722bfa4
Peter <graphhopper@gmx.de>
2022-08-30 14:56:57 +0200

    minor styling for #136

--- src/map/Popup.tsx
+++ src/map/Popup.tsx
@@ -86,6 +86,7 @@ export function PopupComponent({
             >
                 To here
             </button>
+            <hr/>
             <button
                 className={styles.entry}
                 onClick={() => {
