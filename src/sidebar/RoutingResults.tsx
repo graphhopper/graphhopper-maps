0dcbb94719477f7fd378ab876d7034720ea41c0d
Peter <graphhopper@gmx.de>
2022-09-04 00:31:09 +0200

    initial version of GPX export

--- src/sidebar/RoutingResults.tsx
+++ src/sidebar/RoutingResults.tsx
@@ -1,4 +1,4 @@
-import {Path} from '@/api/graphhopper'
+import {Instruction, Path} from '@/api/graphhopper'
 import {CurrentRequest, RequestState, SubRequest} from '@/stores/QueryStore'
 import styles from './RoutingResult.module.css'
 import React, {useEffect, useState} from 'react'
@@ -7,9 +7,11 @@ import {SetSelectedPath} from '@/actions/Actions'
 import {metersToText, milliSecondsToText} from '@/Converters'
 import PlainButton from '@/PlainButton'
 import Details from '@/sidebar/list.svg'
+import GPXDownload from '@/sidebar/file_download.svg'
 import Instructions from '@/sidebar/instructions/Instructions'
 import {useMediaQuery} from 'react-responsive'
 import {tr} from "@/translation/Translation";
+import {Position} from "geojson";
 
 export interface RoutingResultsProps {
     paths: Path[]
@@ -38,7 +40,10 @@ function RoutingResult({path, isSelected}: { path: Path; isSelected: boolean }) 
                         <span className={styles.resultMainText}>{milliSecondsToText(path.time)}</span>
                         <span className={styles.resultSecondaryText}>{metersToText(path.distance)}</span>
                     </div>
-
+                    <PlainButton className={styles.gpxButton} onClick={() => downloadGPX(path)}>
+                        <GPXDownload/>
+                        <div>{tr("Export")}</div>
+                    </PlainButton>
                     {isExpanded
                         ? <PlainButton className={styles.detailsButtonExpanded} onClick={() => setExpanded(false)}>
                             <Details/>
@@ -54,6 +59,44 @@ function RoutingResult({path, isSelected}: { path: Path; isSelected: boolean }) 
             {isExpanded && <Instructions instructions={path.instructions}/>}
         </div>
     )
+}
+
+function downloadGPX(path: Path) {
+    let xmlString = '<?xml version="1.0" encoding="UTF-8" standalone="no" ?><gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" creator="GraphHopper" version="1.1" xmlns:gh="https://graphhopper.com/public/schema/gpx/1.1">\n'
+    xmlString += `<metadata><copyright author="OpenStreetMap contributors"/><link href="http://graphhopper.com"><text>GraphHopper GPX</text></link><time>${new Date().toISOString()}</time></metadata>\n`
+
+    xmlString += path.snapped_waypoints.coordinates.reduce((prevString: string, coord: Position) => {
+        return prevString + `<wpt lat="${coord[1]}" lon="${coord[0]}"></wpt>\n`
+    }, '');
+
+    xmlString += '<rte>\n'
+    xmlString += path.instructions.reduce((prevString: string, instruction: Instruction) => {
+        let routeSegment = `<rtept lat="${instruction.points[0][1].toFixed(6)}" lon="${instruction.points[0][0].toFixed(6)}">`
+        routeSegment += `<desc>${instruction.text}</desc><extensions><gh:distance>${instruction.distance}</gh:distance>`
+        routeSegment += `<gh:time>${instruction.time}</gh:time><gh:sign>${instruction.sign}</gh:sign>`
+        // TODO routeSegment += `<gh:direction>SW</gh:direction><gh:azimuth>222.57</gh:azimuth>` +
+        routeSegment += '</extensions></rtept>\n'
+        return prevString + routeSegment
+    }, '')
+    xmlString += '</rte>\n'
+
+    xmlString += '<trk>\n<name>GraphHopper Track</name><desc></desc>\n<trkseg>'
+    // TODO include time via path.details.time
+    xmlString += path.points.coordinates.reduce((prevString, coord) => {
+        let trackPoint = '<trkpt '
+        trackPoint += `lat="${coord[1].toFixed(6)}" lon="${coord[0].toFixed(6)}">`
+        if (coord.length > 2) trackPoint += `<ele>${coord[2].toFixed(1)}</ele>`
+        trackPoint += '</trkpt>\n'
+        return prevString + trackPoint
+    }, '')
+    xmlString += '</trkseg></trk>\n</gpx>'
+
+    const tmpElement = document.createElement("a");
+    const file = new Blob([xmlString], {type: 'application/gpx+xml'});
+    tmpElement.href = URL.createObjectURL(file);
+    tmpElement.download = "GraphHopper.gpx";
+    document.body.appendChild(tmpElement); // necessary workaround for firefox?
+    tmpElement.click();
 }
 
 function RoutingResultPlacelholder() {
