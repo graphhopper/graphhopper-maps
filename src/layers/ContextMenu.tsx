6163e17964424101348017ee6582ffc07aff7494
easbar <easbar.mail@posteo.net>
2022-10-05 12:34:20 +0200

    map.on is ok?

--- src/layers/ContextMenu.tsx
+++ src/layers/ContextMenu.tsx
@@ -45,7 +45,7 @@ export default function ContextMenu({ map, route, queryPoints }: ContextMenuProp
             map.getTargetElement().addEventListener('touchmove', () => longTouchHandler.onTouchEnd())
             map.getTargetElement().addEventListener('touchend', () => longTouchHandler.onTouchEnd())
 
-            map.getTargetElement().addEventListener('click', () => {
+            map.on('click', () => {
                 overlay?.setPosition(undefined)
                 // setMenuCoordinate(null)
             })
