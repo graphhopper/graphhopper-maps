b5706a1aa24882e4461c8f55a470fa489a1fa032
Peter <graphhopper@gmx.de>
2022-10-08 09:56:57 +0200

    use any directly

--- test/Converters.test.ts
+++ test/Converters.test.ts
@@ -22,19 +22,17 @@ describe('Converters', function () {
         it('default city', function () {
             expect(
                 hitToItem({
-                    ...emptyHit(),
                     name: 'Hoyerswerda - Wojerecy',
                     country: 'Germany',
                     state: 'Saxony',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({ mainText: 'Hoyerswerda - Wojerecy', secondText: '02977, Germany' })
         })
 
         it('default address', function () {
             expect(
                 hitToItem({
-                    ...emptyHit(),
                     name: 'somestreet',
                     country: 'Germany',
                     state: 'Saxony',
@@ -42,21 +40,20 @@ describe('Converters', function () {
                     street: 'somestreet',
                     housenumber: '6',
                     postcode: '10117',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({ mainText: 'somestreet 6', secondText: '10117 Berlin, Germany' })
         })
 
         it('default poi', function () {
             expect(
                 hitToItem({
-                    ...emptyHit(),
                     name: 'Wasserturm Hoyerswerda Bahnhof',
                     country: 'Germany',
                     city: 'Hoyerswerda - Wojerecy',
                     state: 'Saxony',
                     street: 'Am Bahnhofsvorplatz',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({
                 mainText: 'Wasserturm Hoyerswerda Bahnhof',
                 secondText: 'Am Bahnhofsvorplatz, 02977 Hoyerswerda - Wojerecy, Germany',
@@ -66,27 +63,25 @@ describe('Converters', function () {
         it('default poi 2', function () {
             expect(
                 hitToItem({
-                    ...emptyHit(),
                     name: 'An der Schule',
                     country: 'Germany',
                     city: 'Hoyerswerda - Wojerecy',
                     state: 'Saxony',
                     county: 'Bautzen',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({ mainText: 'An der Schule', secondText: '02977 Hoyerswerda - Wojerecy, Germany' })
         })
 
         it('default poi 3', function () {
             expect(
                 hitToItem({
-                    ...emptyHit(),
                     name: 'Am Wasserturm',
                     country: 'Germany',
                     city: 'Hoyerswerda - Wojerecy',
                     state: 'Saxony',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({ mainText: 'Am Wasserturm', secondText: '02977 Hoyerswerda - Wojerecy, Germany' })
         })
     })
@@ -95,21 +90,19 @@ describe('Converters', function () {
         it('nominatim city', function () {
             expect(
                 nominatimHitToItem({
-                    ...emptyHit(),
                     name: '02977 Hoyerswerda - Wojerecy, Germany',
                     country: 'Germany',
                     city: 'Hoyerswerda - Wojerecy',
                     state: 'Saxony',
                     county: 'Bautzen',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({ mainText: '02977 Hoyerswerda - Wojerecy', secondText: 'Germany' })
         })
 
         it('nominatim poi', function () {
             expect(
                 nominatimHitToItem({
-                    ...emptyHit(),
                     name: 'GraphHopper GmbH, Kirchstraße 17, 02977 Hoyerswerda - Wojerecy, Germany',
                     country: 'Germany',
                     city: 'Hoyerswerda - Wojerecy',
@@ -118,7 +111,7 @@ describe('Converters', function () {
                     housenumber: '17',
                     county: 'Bautzen',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({
                 mainText: 'GraphHopper GmbH',
                 secondText: 'Kirchstraße 17, 02977 Hoyerswerda - Wojerecy, Germany',
@@ -128,7 +121,6 @@ describe('Converters', function () {
         it('nominatim poi - ignore name', function () {
             expect(
                 nominatimHitToItem({
-                    ...emptyHit(),
                     name: 'An der Schule, 02977 Hoyerswerda - Wojerecy, Germany',
                     country: 'Germany',
                     city: 'Hoyerswerda - Wojerecy',
@@ -136,41 +128,33 @@ describe('Converters', function () {
                     street: 'An der Schule',
                     county: 'Bautzen',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({ mainText: 'An der Schule', secondText: '02977 Hoyerswerda - Wojerecy, Germany' })
         })
 
         it('nominatim city 2', function () {
             expect(
                 nominatimHitToItem({
-                    ...emptyHit(),
                     name: '02977 Hoyerswerda, Deutschland',
                     country: 'Deutschland',
                     city: 'Hoyerswerda',
                     state: 'Saxony',
                     county: 'Bautzen',
                     postcode: '02977',
-                } as GeocodingHit)
+                } as any)
             ).toEqual({mainText: '02977 Hoyerswerda', secondText: 'Deutschland'})
         })
 
         it('nominatim city 3', function () {
                 expect(
                     nominatimHitToItem({
-                        ...emptyHit(),
                         name: 'Wittensee, something else',
                         country: 'Deutschland',
                         city: 'Groß Wittensee',
                         state: "Schleswig-Holstein",
                         county: "Rendsburg-Eckernförde",
-                    } as GeocodingHit)
+                    } as any)
                 ).toEqual({ mainText: 'Wittensee', secondText: 'Groß Wittensee, Deutschland' })
         })
     })
-
-    function emptyHit(): GeocodingHit {
-        // We do "response.json() as GeocodingHit" in Api which can lead to undefined entries
-        // So we should not just set empty strings
-        return ({} as any) as GeocodingHit
-    }
 })
