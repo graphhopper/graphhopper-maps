5d1b0a5ad4841cb62cbe8226a473d2575e4e2bf8
Peter <graphhopper@gmx.de>
2022-10-14 11:00:12 +0200

    make powered_by logo visible on safari, #237

--- src/sidebar/PoweredBy.tsx
+++ src/sidebar/PoweredBy.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from '@/sidebar/PoweredBy.module.css'
 import Header from '@/sidebar/header.svg'
+import {tr} from "@/translation/Translation";
 
 export default function PoweredBy() {
     return (
         <div className={styles.poweredByContainer}>
-            <span>Powered by </span>
+            <span>{tr('powered_by')}</span>
             <a className={styles.logoContainer} href="https://www.graphhopper.com" target="_blank">
                 <Header />
             </a>
