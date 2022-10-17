c47c56173539bda863c29a3771a71616151521b3
Andi <easbar.mail@posteo.net>
2022-10-17 11:41:24 +0200

    More elongated markers (#257)

--- src/layers/createMarkerSVG.ts
+++ src/layers/createMarkerSVG.ts
@@ -1,5 +1,5 @@
 const MARKER_PATH =
-    'M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z'
+    'M 172.968 632.025 C 26.97 291.031 0 269.413 0 192 C 0 85.961 85.961 0 192 0 S 384 85.961 384 192 C 384 269.413 357.03 291.031 213 632 C 193.781 667.361 192.57 667.361 172.968 632.025 Z'
 const INNER_CIRCLE = 'M192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z'
 
 interface MarkerProps {
@@ -8,10 +8,10 @@ interface MarkerProps {
     size?: number
 }
 
-// todo: this is mostly duplicated from `Marker.tsx`. To use `Marker.tsx` we would probably need to add ol.Overlays, i.e.
-//       create a div for each marker and insert the svg from `Marker.tsx`.
+// todo: this is mostly duplicated from `Marker.tsx`, but we use a more elongated shape (MARKER_PATH).
+//       To use `Marker.tsx` we would probably need to add ol.Overlays, i.e. create a div for each marker and insert the svg from `Marker.tsx`.
 export function createSvg({ color, number, size = 0 }: MarkerProps) {
-    return `<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="${
+    return `<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 658" width="${
         // todo: we do not use width in Marker.tsx, but without this the markers are not shown in Firefox :( (but they are shown in Chrome...)
         size
     }" height="${size}" style="stroke: none; fill: ${hexToRgb(color)};">
@@ -19,8 +19,7 @@ export function createSvg({ color, number, size = 0 }: MarkerProps) {
         number === undefined
             ? '<path d="' + INNER_CIRCLE + '" fill="white" />'
             : '<circle cx="192" cy="190" r="120" fill="white" />'
-    }<text x="50%" y="47%" text-anchor="middle" style="font-size: ${150 + 'px'}" fill="black">${
-        // todo: the text is not shown in Firefox (only in Chrome) :(
+    }<text x="50%" y="40%" text-anchor="middle" style="font-size: ${180 + 'px'}" fill="black">${
         number !== undefined ? number : ''
     }</text></svg>`
 }
