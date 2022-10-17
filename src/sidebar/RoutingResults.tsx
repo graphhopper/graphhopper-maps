d29944880c8c6bd76b59a4a27807643f3f128bc8
Peter <graphhopper@gmx.de>
2022-10-17 11:24:55 +0200

    fix #256

--- src/sidebar/RoutingResults.tsx
+++ src/sidebar/RoutingResults.tsx
@@ -107,7 +107,7 @@ function RoutingResult({ path, isSelected, profile }: { path: Path; isSelected: 
 }
 
 function containsBadTracks(details: [number, number, string][]) {
-    if (!details) return false
+    if (!details || details.length == 0) return false
     for (let i in details) {
         if (details[i][2] == 'grade2') return true
         if (details[i][2] == 'grade3') return true
@@ -118,7 +118,7 @@ function containsBadTracks(details: [number, number, string][]) {
 }
 
 function crossesBorder(countryPathDetail: [number, number, string][]) {
-    if (!countryPathDetail) return false
+    if (!countryPathDetail || countryPathDetail.length == 0) return false
     const init = countryPathDetail[0][2]
     for (let i in countryPathDetail) {
         if (countryPathDetail[i][2] != init) return true
@@ -127,7 +127,7 @@ function crossesBorder(countryPathDetail: [number, number, string][]) {
 }
 
 function containsValue(details: [number, number, string][], value: string) {
-    if (!details) return false
+    if (!details || details.length == 0) return false
     for (let i in details) {
         if (details[i][2] == value) return true
     }
