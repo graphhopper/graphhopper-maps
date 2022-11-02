122ceb3ed24550bf9de28d2f5a7932ffe8a6e181
easbar <easbar.mail@posteo.net>
2022-11-02 18:59:27 +0100

    Move text-point handling one level up
    * all state changes are on the same level now

--- src/NavBar.ts
+++ src/NavBar.ts
@@ -41,8 +41,8 @@ export default class NavBar {
         return coordinate === point.queryText ? coordinate : coordinate + '_' + point.queryText
     }
 
-    private parsePoints(url: URL, profile: RoutingProfile): QueryPoint[] {
-        const points = url.searchParams.getAll('point').map((parameter, idx) => {
+    private static parsePoints(url: URL): QueryPoint[] {
+        return url.searchParams.getAll('point').map((parameter, idx) => {
             const split = parameter.split('_')
 
             const point = {
@@ -64,35 +64,6 @@ export default class NavBar {
 
             return point
         })
-
-        // support legacy URLs without coordinates (not initialized) and only text, see #199
-        if (points.some(p => !p.isInitialized && p.queryText.length > 0)) {
-            if (!profile.name) profile = { name: 'car' }
-            let fullyInitPoints: QueryPoint[] = Array.from({ length: points.length })
-            points.forEach((p, idx) => {
-                if (p.isInitialized) fullyInitPoints[idx] = p
-                else
-                    getApi()
-                        .geocode(p.queryText, 'nominatim')
-                        .then(res => {
-                            if (res.hits.length <= 0) return
-                            fullyInitPoints[idx] = {
-                                ...p,
-                                queryText: res.hits[0].name,
-                                coordinate: { lat: res.hits[0].point.lat, lng: res.hits[0].point.lng },
-                                isInitialized: true,
-                            }
-                            if (fullyInitPoints.every(p => p.isInitialized)) {
-                                if (fullyInitPoints.length <= 2) this.fillPoints(fullyInitPoints)
-                                Dispatcher.dispatch(new SetRoutingParametersAtOnce(fullyInitPoints, profile))
-                            }
-                        })
-            })
-            return [] // skip normal SetRoutingParametersAtOnce
-        }
-
-        // this ensures that if no or one point parameter is specified in the URL there are still two input fields
-        return points.length > 2 ? points : this.fillPoints(points)
     }
 
     private static parseCoordinate(params: string) {
@@ -125,15 +96,43 @@ export default class NavBar {
         const url = new URL(window.location.href)
 
         const parsedProfileName = NavBar.parseProfile(url)
-        const profile = parsedProfileName ? { name: parsedProfileName } : this.queryStore.state.routingProfile
-        const parsedPoints = this.parsePoints(url, profile)
+        let profile = parsedProfileName ? { name: parsedProfileName } : this.queryStore.state.routingProfile
+        let parsedPoints = NavBar.parsePoints(url)
 
-        // estimate map bounds from url points if there are any. this way we prevent loading tiles for the world view
-        // only to zoom to the route shortly after
-        const bbox = this.getBBoxFromUrlPoints(parsedPoints.map(p => p.coordinate))
-        if (bbox) Dispatcher.dispatch(new SetInitialBBox(bbox))
+        // support legacy URLs without coordinates (not initialized) and only text, see #199
+        if (parsedPoints.some(p => !p.isInitialized && p.queryText.length > 0)) {
+            if (!profile.name) profile = { name: 'car' }
+            let fullyInitPoints: QueryPoint[] = Array.from({ length: parsedPoints.length })
+            parsedPoints.forEach((p, idx) => {
+                if (p.isInitialized) fullyInitPoints[idx] = p
+                else
+                    getApi()
+                        .geocode(p.queryText, 'nominatim')
+                        .then(res => {
+                            if (res.hits.length <= 0) return
+                            fullyInitPoints[idx] = {
+                                ...p,
+                                queryText: res.hits[0].name,
+                                coordinate: { lat: res.hits[0].point.lat, lng: res.hits[0].point.lng },
+                                isInitialized: true,
+                            }
+                            if (fullyInitPoints.every(p => p.isInitialized)) {
+                                if (fullyInitPoints.length <= 2) this.fillPoints(fullyInitPoints)
+                                Dispatcher.dispatch(new SetRoutingParametersAtOnce(fullyInitPoints, profile))
+                            }
+                        })
+            })
+        } else {
+            // this ensures that if no or one point parameter is specified in the URL there are still two input fields
+            if (parsedPoints.length > 2) parsedPoints = this.fillPoints(parsedPoints)
 
-        if (parsedPoints.length > 0) Dispatcher.dispatch(new SetRoutingParametersAtOnce(parsedPoints, profile))
+            // estimate map bounds from url points if there are any. this way we prevent loading tiles for the world view
+            // only to zoom to the route shortly after
+            const bbox = NavBar.getBBoxFromUrlPoints(parsedPoints.map(p => p.coordinate))
+            if (bbox) Dispatcher.dispatch(new SetInitialBBox(bbox))
+
+            if (parsedPoints.length > 0) Dispatcher.dispatch(new SetRoutingParametersAtOnce(parsedPoints, profile))
+        }
 
         // add map style
         const parsedStyleOption = this.parseLayer(url)
@@ -165,7 +164,7 @@ export default class NavBar {
         if (newHref !== window.location.href) window.history.pushState('last state', '', newHref)
     }
 
-    private getBBoxFromUrlPoints(urlPoints: Coordinate[]): Bbox | null {
+    private static getBBoxFromUrlPoints(urlPoints: Coordinate[]): Bbox | null {
         const bbox: Bbox = urlPoints.reduce(
             (res: Bbox, c) => [
                 Math.min(res[0], c.lng),
