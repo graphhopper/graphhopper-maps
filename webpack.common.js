b7151d62aebfc3f855375b03c91bf0fdce529f8d
Peter <graphhopper@gmx.de>
2022-11-30 09:08:47 +0100

    make cache friendly #295

--- webpack.common.js
+++ webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
     },
     resolve: {
         alias: {
