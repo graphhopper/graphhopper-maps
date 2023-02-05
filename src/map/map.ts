a022d6f104b929e007f46b8e6e498836b59e49c5
Peter <graphhopper@gmx.de>
2023-02-05 13:21:33 +0100

    comment

--- src/map/map.ts
+++ src/map/map.ts
@@ -17,7 +17,9 @@ export function createMap(): Map {
             center: fromLonLat([10, 10]),
             zoom: 2,
         }),
-        interactions: defaults({ pinchRotate: false }),
+        interactions: defaults({
+            pinchRotate: false, // ugly on mobile as we don't have a button to reset to 0 yet
+        }),
         controls: defaultControls({
             rotate: false, // for now also disable this extra button
             zoom: true,
