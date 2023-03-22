29a99f2782caee0288ae8359f8b9371e140b4b0c
easbar <easbar.mail@posteo.net>
2023-03-22 21:01:31 +0100

    format

--- src/sidebar/CustomModelExamples.ts
+++ src/sidebar/CustomModelExamples.ts
@@ -7,17 +7,17 @@ export const customModelExamples: { [key: string]: CustomModel } = {
         speed: [],
         areas: {
             type: 'FeatureCollection',
-            features: []
-        }
+            features: [],
+        },
     },
     exclude_motorway: {
-        priority: [{ if: 'road_class == MOTORWAY', multiply_by: '0.0' }]
+        priority: [{ if: 'road_class == MOTORWAY', multiply_by: '0.0' }],
     },
     limit_speed: {
         speed: [
             { if: 'true', limit_to: '100' },
-            { if: 'road_class == TERTIARY', limit_to: '80' }
-        ]
+            { if: 'road_class == TERTIARY', limit_to: '80' },
+        ],
     },
     exclude_area: {
         priority: [{ if: 'in_berlin_bbox', multiply_by: '0' }],
@@ -36,26 +36,26 @@ export const customModelExamples: { [key: string]: CustomModel } = {
                                 [13.228, 52.437],
                                 [13.579, 52.447],
                                 [13.563, 52.609],
-                                [13.253, 52.608]
-                            ]
-                        ]
-                    }
-                }
-            ]
-        }
+                                [13.253, 52.608],
+                            ],
+                        ],
+                    },
+                },
+            ],
+        },
     },
     cargo_bike: {
         speed: [{ if: 'road_class == TRACK', limit_to: '2' }],
-        priority: [{ if: 'max_width < 1.5 || road_class == STEPS', multiply_by: '0' }]
+        priority: [{ if: 'max_width < 1.5 || road_class == STEPS', multiply_by: '0' }],
     },
     combined: {
         distance_influence: 100,
         speed: [{ if: 'road_class == TRACK || road_environment == FERRY || surface == DIRT', limit_to: '10' }],
         priority: [
             { if: 'road_environment == TUNNEL || toll == ALL', multiply_by: '0.5' },
-            { if: 'max_weight < 3 || max_height < 2.5', multiply_by: '0.0' }
-        ]
-    }
+            { if: 'max_weight < 3 || max_height < 2.5', multiply_by: '0.0' },
+        ],
+    },
 }
 
 export function customModel2prettyString(customModel: CustomModel) {
