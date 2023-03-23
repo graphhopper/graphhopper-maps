f04c3be7b44074c1386744a409e7ff8c69271ff7
Peter <graphhopper@gmx.de>
2023-03-23 15:01:20 +0100

    format

--- src/sidebar/SettingsBox.tsx
+++ src/sidebar/SettingsBox.tsx
@@ -1,29 +1,26 @@
-import {ToggleDistanceUnits} from '@/actions/Actions'
+import { ToggleDistanceUnits } from '@/actions/Actions'
 import Dispatcher from '@/stores/Dispatcher'
 import styles from '@/sidebar/SettingsBox.module.css'
-import {tr} from '@/translation/Translation'
+import { tr } from '@/translation/Translation'
 import PlainButton from '@/PlainButton'
 import OnIcon from '@/sidebar/toggle_on.svg'
 import OffIcon from '@/sidebar/toggle_off.svg'
-import {useContext} from 'react'
-import {ShowDistanceInMilesContext} from '@/ShowDistanceInMilesContext'
+import { useContext } from 'react'
+import { ShowDistanceInMilesContext } from '@/ShowDistanceInMilesContext'
 
 export default function SettingsBox() {
     const showDistanceInMiles = useContext(ShowDistanceInMilesContext)
     return (
-
         <div className={styles.parent}>
-            <div className={styles.title}>
-                {tr('settings')}
-            </div>
+            <div className={styles.title}>{tr('settings')}</div>
             <div className={styles.settingsTable}>
                 <PlainButton
-                    style={{color: showDistanceInMiles ? '' : 'lightgray'}} // todonow: move to css?
+                    style={{ color: showDistanceInMiles ? '' : 'lightgray' }} // todonow: move to css?
                     onClick={() => Dispatcher.dispatch(new ToggleDistanceUnits())}
                 >
-                    {showDistanceInMiles ? <OnIcon/> : <OffIcon/>}
+                    {showDistanceInMiles ? <OnIcon /> : <OffIcon />}
                 </PlainButton>
-                <div style={{color: showDistanceInMiles ? '#5b616a' : 'gray'}}>
+                <div style={{ color: showDistanceInMiles ? '#5b616a' : 'gray' }}>
                     {tr('distance_unit', [tr(showDistanceInMiles ? 'mi' : 'km')])}
                 </div>
             </div>
