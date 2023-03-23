47201f79c9fbe1a712522f3c7b6857b234e9ba5c
easbar <easbar.mail@posteo.net>
2023-03-23 07:12:44 +0100

    Move custom model examples to separate file

--- src/sidebar/CustomModelBox.tsx
+++ src/sidebar/CustomModelBox.tsx
@@ -7,68 +7,10 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 import { create } from 'custom-model-editor/src/index'
 import Dispatcher from '@/stores/Dispatcher'
 import { SetCustomModel } from '@/actions/Actions'
-import { CustomModel } from '@/stores/QueryStore'
 import { tr } from '@/translation/Translation'
 import PlainButton from '@/PlainButton'
 import { Settings } from '@/stores/SettingsStore'
-
-const examples: { [key: string]: CustomModel } = {
-    default_example: {
-        distance_influence: 15,
-        priority: [{ if: 'road_environment == FERRY', multiply_by: '0.9' }],
-        speed: [],
-        areas: {
-            type: 'FeatureCollection',
-            features: [],
-        },
-    },
-    exclude_motorway: {
-        priority: [{ if: 'road_class == MOTORWAY', multiply_by: '0.0' }],
-    },
-    limit_speed: {
-        speed: [
-            { if: 'true', limit_to: '100' },
-            { if: 'road_class == TERTIARY', limit_to: '80' },
-        ],
-    },
-    exclude_area: {
-        priority: [{ if: 'in_berlin_bbox', multiply_by: '0' }],
-        areas: {
-            type: 'FeatureCollection',
-            features: [
-                {
-                    type: 'Feature',
-                    id: 'berlin_bbox',
-                    properties: {},
-                    geometry: {
-                        type: 'Polygon',
-                        coordinates: [
-                            [
-                                [13.253, 52.608],
-                                [13.228, 52.437],
-                                [13.579, 52.447],
-                                [13.563, 52.609],
-                                [13.253, 52.608],
-                            ],
-                        ],
-                    },
-                },
-            ],
-        },
-    },
-    cargo_bike: {
-        speed: [{ if: 'road_class == TRACK', limit_to: '2' }],
-        priority: [{ if: 'max_width < 1.5 || road_class == STEPS', multiply_by: '0' }],
-    },
-    combined: {
-        distance_influence: 100,
-        speed: [{ if: 'road_class == TRACK || road_environment == FERRY || surface == DIRT', limit_to: '10' }],
-        priority: [
-            { if: 'road_environment == TUNNEL || toll == ALL', multiply_by: '0.5' },
-            { if: 'max_weight < 3 || max_height < 2.5', multiply_by: '0.0' },
-        ],
-    },
-}
+import { customModel2prettyString, customModelExamples } from '@/sidebar/CustomModelBoxExamples'
 
 function convertEV(encodedValues: object[]): any {
     const categories: any = {}
@@ -113,7 +55,7 @@ export default function CustomModelBox({ encodedValues, queryOngoing, settings }
                 instance.value = initialCustomModelStr
             }
         } else {
-            instance.value = customModel2prettyString(examples['default_example'])
+            instance.value = customModel2prettyString(customModelExamples['default_example'])
             dispatchCustomModel(instance.value, true, true)
         }
 
@@ -163,10 +105,10 @@ export default function CustomModelBox({ encodedValues, queryOngoing, settings }
                 <select
                     className={styles.examples}
                     onChange={(e: any) => {
-                        editor.value = customModel2prettyString(examples[e.target.value])
+                        editor.value = customModel2prettyString(customModelExamples[e.target.value])
                         // When selecting an example we request a routing request and act like the model is valid,
                         // even when it is not according to the editor validation.
-                        dispatchCustomModel(JSON.stringify(examples[e.target.value]), true, true)
+                        dispatchCustomModel(JSON.stringify(customModelExamples[e.target.value]), true, true)
                     }}
                 >
                     <option value="default_example">{tr('examples_custom_model')}</option>
@@ -212,8 +154,4 @@ function dispatchCustomModel(customModelString: string, isValid: boolean, withRo
     } catch (e) {
         Dispatcher.dispatch(new SetCustomModel(null, false, withRouteRequest))
     }
-}
-
-function customModel2prettyString(customModel: CustomModel) {
-    return JSON.stringify(customModel, null, 2)
 }
