a592defb692da65e100ca4f8097a1345a0a729cb
oblonski <stefan.schroeder@graphhopper.com>
2023-03-29 08:40:27 +0200

    replace var by const and format

--- src/layers/UsePathsLayer.tsx
+++ src/layers/UsePathsLayer.tsx
@@ -1,4 +1,4 @@
-import {Feature, Map} from 'ol'
+import { Feature, Map } from 'ol'
 import { Path } from '@/api/graphhopper'
 import { FeatureCollection } from 'geojson'
 import { useEffect } from 'react'
@@ -12,9 +12,9 @@ import { click } from 'ol/events/condition'
 import Dispatcher from '@/stores/Dispatcher'
 import { SetSelectedPath } from '@/actions/Actions'
 import { SelectEvent } from 'ol/interaction/Select'
-import {QueryPoint} from "@/stores/QueryStore";
-import {Coordinate, distance} from "ol/coordinate";
-import LineString from "ol/geom/LineString";
+import { QueryPoint } from '@/stores/QueryStore'
+import { Coordinate, distance } from 'ol/coordinate'
+import LineString from 'ol/geom/LineString'
 
 const pathsLayerKey = 'pathsLayer'
 const selectedPathLayerKey = 'selectedPathLayer'
@@ -78,31 +78,31 @@ function addUnselectedPathsLayer(map: Map, paths: Path[]) {
     map.addInteraction(select)
 }
 
-function createBezierLineString(start: number[], end: number[]):LineString {
-    var curvePoints = [];
-    var center = [(start[0] + end[0]) / 2, (start[1] + end[1]) / 2];
-    var radius = distance(start, end) / 2;
+function createBezierLineString(start: number[], end: number[]): LineString {
+    const bezierPoints = []
+    const center = [(start[0] + end[0]) / 2, (start[1] + end[1]) / 2]
+    const radius = distance(start, end) / 2
 
-    var startAngle = Math.atan2(start[1] - center[1], start[0] - center[0]);
-    var endAngle = Math.atan2(end[1] - center[1], end[0] - center[0]);
+    const startAngle = Math.atan2(start[1] - center[1], start[0] - center[0])
+    const endAngle = Math.atan2(end[1] - center[1], end[0] - center[0])
 
     // Define the control points for the Bezier curve
-    var controlPoints = [
-        center[0] + 1/2 * radius * Math.sin(startAngle + 1/2 * (endAngle - startAngle)),
-        center[1] + 1/2 * radius * Math.cos(startAngle + 1/2 * (endAngle - startAngle))
-    ];
+    const controlPoints = [
+        center[0] + (1 / 2) * radius * Math.sin(startAngle + (1 / 2) * (endAngle - startAngle)),
+        center[1] + (1 / 2) * radius * Math.cos(startAngle + (1 / 2) * (endAngle - startAngle)),
+    ]
 
     // Calculate intermediate points along the curve using a Bezier curve
-    curvePoints.push(start);
-    for (var t = 0; t <= 1; t += 0.1) {
-        var point = [
-            (1-t)*(1-t)*start[0] + 2*t*(1-t)*controlPoints[0] + t*t*end[0],
-            (1-t)*(1-t)*start[1] + 2*t*(1-t)*controlPoints[1] + t*t*end[1]
-        ];
-        curvePoints.push(point);
+    bezierPoints.push(start)
+    for (let t = 0; t <= 1; t += 0.1) {
+        const point = [
+            (1 - t) * (1 - t) * start[0] + 2 * t * (1 - t) * controlPoints[0] + t * t * end[0],
+            (1 - t) * (1 - t) * start[1] + 2 * t * (1 - t) * controlPoints[1] + t * t * end[1],
+        ]
+        bezierPoints.push(point)
     }
-    curvePoints.push(end)
-    return new LineString(curvePoints);
+    bezierPoints.push(end)
+    return new LineString(bezierPoints)
 }
 
 function addAccessNetworkLayer(map: Map, selectedPath: Path, queryPoints: QueryPoint[]) {
@@ -113,17 +113,17 @@ function addAccessNetworkLayer(map: Map, selectedPath: Path, queryPoints: QueryP
             width: 4,
             lineDash: [1, 10],
             lineCap: 'round',
-            lineJoin: 'round'
+            lineJoin: 'round',
         }),
     })
     var layer = new VectorLayer({
-        source: new VectorSource()
-    });
-    layer.setStyle(style);
-    for(let i=0;i<selectedPath.snapped_waypoints.coordinates.length;i++){
-        let start = fromLonLat([queryPoints[i].coordinate.lng,queryPoints[i].coordinate.lat]);
-        let end = fromLonLat(selectedPath.snapped_waypoints.coordinates[i]);
-        layer.getSource()?.addFeature(new Feature(createBezierLineString(start,end)))
+        source: new VectorSource(),
+    })
+    layer.setStyle(style)
+    for (let i = 0; i < selectedPath.snapped_waypoints.coordinates.length; i++) {
+        let start = fromLonLat([queryPoints[i].coordinate.lng, queryPoints[i].coordinate.lat])
+        let end = fromLonLat(selectedPath.snapped_waypoints.coordinates[i])
+        layer.getSource()?.addFeature(new Feature(createBezierLineString(start, end)))
     }
     layer.set(accessNetworkLayerKey, true)
     layer.setZIndex(1)
