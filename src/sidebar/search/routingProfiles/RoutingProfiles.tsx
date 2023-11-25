a1d80f21b470969a45b5a05e8dd325ea77d39b28
Sachsenspielt <sachsenspielt3@gmail.com>
2023-11-25 21:33:27 +0100

    early prototype

--- src/sidebar/search/routingProfiles/RoutingProfiles.tsx
+++ src/sidebar/search/routingProfiles/RoutingProfiles.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './RoutingProfiles.module.css'
 import Dispatcher from '@/stores/Dispatcher'
 import { SetVehicleProfile } from '@/actions/Actions'
@@ -15,6 +15,8 @@ import ScooterIcon from './scooter.svg'
 import SmallTruckIcon from './small_truck.svg'
 import TruckIcon from './truck.svg'
 import WheelchairIcon from './wheelchair.svg'
+import QuestionMarkIcon from './question_mark.svg'
+import Chevron from './chevron.svg'
 import { tr } from '@/translation/Translation'
 import CustomModelBoxSVG from '@/sidebar/open_custom_model.svg'
 
@@ -31,6 +33,29 @@ export default function ({
     toggleCustomModelBox: () => void
     customModelBoxEnabled: boolean
 }) {
+    const [profileShowIndex, setProfileShowIndex] = useState(1)
+
+    useEffect(() => {
+        console.log('profileShowIndex', profileShowIndex)
+    }, [profileShowIndex])
+
+    function move(amount: number) {
+        const index = profileShowIndex + amount
+        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
+        if (!profilesCarouselItems) return
+        profilesCarouselItems.scrollBy({
+            left: profilesCarouselItems.clientWidth * index - profilesCarouselItems.clientWidth * profileShowIndex,
+            behavior: 'smooth',
+        })
+        setProfileShowIndex(index)
+    }
+
+    function maxIndex() {
+        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
+        if (!profilesCarouselItems) return 1
+        return Math.round(profilesCarouselItems.scrollWidth / profilesCarouselItems.clientWidth)
+    }
+
     return (
         <div className={styles.profilesParent}>
             <PlainButton
@@ -40,28 +65,46 @@ export default function ({
             >
                 <CustomModelBoxSVG />
             </PlainButton>
-            <ul className={styles.profiles}>
-                {routingProfiles.map(profile => {
-                    const className =
-                        profile.name === selectedProfile.name
-                            ? styles.selectedProfile + ' ' + styles.profileBtn
-                            : styles.profileBtn
-                    return (
-                        <li key={profile.name}>
-                            <PlainButton
-                                title={tr(profile.name)}
-                                onClick={() => Dispatcher.dispatch(new SetVehicleProfile(profile))}
-                                className={className}
-                            >
-                                {customModelBoxEnabled && profile.name === selectedProfile.name && (
-                                    <CustomModelBoxSVG className={styles.asIndicator} />
-                                )}
-                                {getIcon(profile)}
-                            </PlainButton>
-                        </li>
-                    )
-                })}
-            </ul>
+            <div className={styles.carousel}>
+                <PlainButton
+                    className={styles.chevron}
+                    title="go left"
+                    onClick={() => move(-1)}
+                    disabled={profileShowIndex === 1}
+                >
+                    <Chevron />
+                </PlainButton>
+                <ul className={styles.profiles} id="profiles_carousel_items">
+                    {routingProfiles.map(profile => {
+                        const className =
+                            profile.name === selectedProfile.name
+                                ? styles.selectedProfile + ' ' + styles.profileBtn
+                                : styles.profileBtn
+                        return (
+                            <li key={profile.name}>
+                                <PlainButton
+                                    title={tr(profile.name)}
+                                    onClick={() => Dispatcher.dispatch(new SetVehicleProfile(profile))}
+                                    className={className}
+                                >
+                                    {customModelBoxEnabled && profile.name === selectedProfile.name && (
+                                        <CustomModelBoxSVG className={styles.asIndicator} />
+                                    )}
+                                    {getIcon(profile)}
+                                </PlainButton>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <PlainButton
+                    className={styles.chevron + ' ' + styles.flip}
+                    title="go right"
+                    onClick={() => move(1)}
+                    disabled={profileShowIndex === maxIndex()}
+                >
+                    <Chevron />
+                </PlainButton>
+            </div>
         </div>
     )
 }
@@ -91,6 +134,6 @@ function getIcon(profile: RoutingProfile) {
         case 'wheelchair':
             return <WheelchairIcon />
         default:
-            return profile.name
+            return <QuestionMarkIcon />
     }
 }
