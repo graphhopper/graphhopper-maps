88f4a4c2534e46940e79ecb2fa8f4271cea1167b
Finn | Sachsenspielt <59836848+SachsenspieltCoding@users.noreply.github.com>
2024-01-16 20:47:54 +0100

    New profiles carousel (#368)
    * added translations
    
    * added german translation for towerslope
    
    * update german translations
    
    * early prototype
    
    * reworked, more stable
    
    * changed translation keys
    
    * update translations
    
    * fix for webkit, limit scrollBy
    
    * refactor, changed param of move to bool

--- src/sidebar/search/routingProfiles/RoutingProfiles.tsx
+++ src/sidebar/search/routingProfiles/RoutingProfiles.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './RoutingProfiles.module.css'
 import Dispatcher from '@/stores/Dispatcher'
 import { SetVehicleProfile } from '@/actions/Actions'
@@ -15,6 +15,8 @@ import ScooterIcon from './scooter.svg'
 import SmallTruckIcon from './small_truck.svg'
 import TruckIcon from './truck.svg'
 import WheelchairIcon from './wheelchair.svg'
+import QuestionMarkIcon from './question_mark.svg'
+import Chevron from './chevron.svg'
 import { tr } from '@/translation/Translation'
 import CustomModelBoxSVG from '@/sidebar/open_custom_model.svg'
 
@@ -31,6 +33,54 @@ export default function ({
     toggleCustomModelBox: () => void
     customModelBoxEnabled: boolean
 }) {
+    const [profileScroll, setProfileScroll] = useState(0)
+    const [profileWidth, setProfileWidth] = useState(0)
+
+    function handleResize() {
+        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
+        if (!profilesCarouselItems) return
+        if (profilesCarouselItems.scrollWidth > profilesCarouselItems.clientWidth) {
+            for (const chevron of document.getElementsByClassName(styles.chevron)) {
+                chevron.classList.add(styles.enabled)
+            }
+            profilesCarouselItems.classList.remove(styles.profiles_center)
+        } else {
+            for (const chevron of document.getElementsByClassName(styles.chevron)) {
+                chevron.classList.remove(styles.enabled)
+            }
+            profilesCarouselItems.classList.add(styles.profiles_center)
+        }
+        setProfileWidth(profilesCarouselItems.scrollWidth - profilesCarouselItems.clientWidth)
+    }
+
+    useEffect(() => {
+        handleResize()
+        // If the window is resized, we need to check if the chevrons should be enabled
+        // and if the profiles should be centered
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [routingProfiles])
+
+    function move(forward: boolean = true) {
+        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
+        if (!profilesCarouselItems) return
+
+        const scrollAmount = profilesCarouselItems.clientWidth * (forward ? 1 : -1)
+        const limit = forward ? profileWidth - profileScroll : -profileScroll
+        const scrollValue = forward ? Math.min(scrollAmount, limit) : Math.max(scrollAmount, limit)
+
+        profilesCarouselItems.scrollBy({
+            left: scrollValue,
+            behavior: 'smooth',
+        })
+    }
+
+    function onScroll() {
+        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
+        if (!profilesCarouselItems) return
+        setProfileScroll(profilesCarouselItems.scrollLeft)
+    }
+
     return (
         <div className={styles.profilesParent}>
             <PlainButton
@@ -40,28 +90,46 @@ export default function ({
             >
                 <CustomModelBoxSVG />
             </PlainButton>
-            <ul className={styles.profiles}>
-                {routingProfiles.map(profile => {
-                    const className =
-                        profile.name === selectedProfile.name
-                            ? styles.selectedProfile + ' ' + styles.profileBtn
-                            : styles.profileBtn
-                    return (
-                        <li key={profile.name}>
-                            <PlainButton
-                                title={tr(profile.name)}
-                                onClick={() => Dispatcher.dispatch(new SetVehicleProfile(profile))}
-                                className={className}
-                            >
-                                {customModelBoxEnabled && profile.name === selectedProfile.name && (
-                                    <CustomModelBoxSVG className={styles.asIndicator} />
-                                )}
-                                {getIcon(profile)}
-                            </PlainButton>
-                        </li>
-                    )
-                })}
-            </ul>
+            <div className={styles.carousel}>
+                <PlainButton
+                    className={styles.chevron}
+                    title={tr('back')}
+                    onClick={() => move(false)}
+                    disabled={profileScroll <= 0}
+                >
+                    <Chevron />
+                </PlainButton>
+                <ul className={styles.profiles} id="profiles_carousel_items" onScroll={onScroll}>
+                    {routingProfiles.map(profile => {
+                        const className =
+                            profile.name === selectedProfile.name
+                                ? styles.selectedProfile + ' ' + styles.profileBtn
+                                : styles.profileBtn
+                        return (
+                            <li key={profile.name}>
+                                <PlainButton
+                                    title={tr(profile.name)}
+                                    onClick={() => Dispatcher.dispatch(new SetVehicleProfile(profile))}
+                                    className={className}
+                                >
+                                    {customModelBoxEnabled && profile.name === selectedProfile.name && (
+                                        <CustomModelBoxSVG className={styles.asIndicator} />
+                                    )}
+                                    {getIcon(profile)}
+                                </PlainButton>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <PlainButton
+                    className={styles.chevron + ' ' + styles.flip}
+                    title={tr('next')}
+                    onClick={() => move()}
+                    disabled={profileScroll >= profileWidth}
+                >
+                    <Chevron />
+                </PlainButton>
+            </div>
         </div>
     )
 }
@@ -91,6 +159,6 @@ function getIcon(profile: RoutingProfile) {
         case 'wheelchair':
             return <WheelchairIcon />
         default:
-            return profile.name
+            return <QuestionMarkIcon />
     }
 }
