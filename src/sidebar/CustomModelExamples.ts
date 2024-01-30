43edb3a9f54e188488acba84ad5a2da3c1c07847
Peter <graphhopper@gmx.de>
2024-01-30 12:31:08 +0100

    format

--- src/sidebar/CustomModelExamples.ts
+++ src/sidebar/CustomModelExamples.ts
@@ -14,36 +14,44 @@ export const customModelExamples: { [key: string]: CustomModel } = {
         priority: [{ if: 'road_class == MOTORWAY', multiply_by: '0.0' }],
     },
     avoid_tunnel_bridges: {
-        priority: [{ if: "road_environment == TUNNEL || road_environment == BRIDGE", multiply_by: '0.1' }]
+        priority: [{ if: 'road_environment == TUNNEL || road_environment == BRIDGE', multiply_by: '0.1' }],
     },
     exclude_disneyland_paris: {
         priority: [
-            { if: "road_environment==FERRY", multiply_by: "0.9" },
-            { if: "in_area1", multiply_by: "0" },
+            { if: 'road_environment==FERRY', multiply_by: '0.9' },
+            { if: 'in_area1', multiply_by: '0' },
         ],
         areas: {
-            type: "FeatureCollection",
-            features: [{
-                id: "area1",
-                properties: {},
-                type: "Feature",
-                geometry: {
-                    type: "Polygon",
-                    coordinates: [[[2.74773,48.876539],[2.751936,48.794903],[2.850813,48.819775],[2.808754,48.887642],[2.74773,48.876539]]]
+            type: 'FeatureCollection',
+            features: [
+                {
+                    id: 'area1',
+                    properties: {},
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Polygon',
+                        coordinates: [
+                            [
+                                [2.74773, 48.876539],
+                                [2.751936, 48.794903],
+                                [2.850813, 48.819775],
+                                [2.808754, 48.887642],
+                                [2.74773, 48.876539],
+                            ],
+                        ],
+                    },
                 },
-            }]
-        }
+            ],
+        },
     },
     electric_car: {
         distance_influence: 100,
         priority: [
-            { if:      "average_slope >= 10", "multiply_by": "0.7"},
-            { else_if: "average_slope >=  7", "multiply_by": "0.8"},
-            { else_if: "average_slope >=  4", "multiply_by": "0.9"},
+            { if: 'average_slope >= 10', multiply_by: '0.7' },
+            { else_if: 'average_slope >=  7', multiply_by: '0.8' },
+            { else_if: 'average_slope >=  4', multiply_by: '0.9' },
         ],
-        speed: [
-            { if: "true", "limit_to": "110" },
-        ]
+        speed: [{ if: 'true', limit_to: '110' }],
     },
     limit_speed: {
         speed: [
