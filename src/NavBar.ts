c9ef18dffeb3d72ed76c6a203fd43d620fe43f16
Peter <graphhopper@gmx.de>
2024-06-15 22:00:24 +0200

    Revert 'cosmetics: avoid layer and profile parameter on first load if equal to default' fixes #395

--- src/NavBar.ts
+++ src/NavBar.ts
@@ -13,7 +13,6 @@ import QueryStore, {
 } from '@/stores/QueryStore'
 import MapOptionsStore, { MapOptionsStoreState } from './stores/MapOptionsStore'
 import { getApi } from '@/api/Api'
-import config from 'config'
 
 export default class NavBar {
     private readonly queryStore: QueryStore
@@ -28,17 +27,12 @@ export default class NavBar {
 
     async startSyncingUrlWithAppState() {
         // our first history entry shall be the one that we end up with when the app loads for the first time
-        window.history.replaceState(null, '', this.createUrlFromState(true))
+        window.history.replaceState(null, '', this.createUrlFromState())
         this.queryStore.register(() => this.updateUrlFromState())
         this.mapStore.register(() => this.updateUrlFromState())
     }
 
-    private static createUrl(
-        baseUrl: string,
-        queryStoreState: QueryStoreState,
-        mapState: MapOptionsStoreState,
-        first: boolean
-    ) {
+    private static createUrl(baseUrl: string, queryStoreState: QueryStoreState, mapState: MapOptionsStoreState) {
         const result = new URL(baseUrl)
         if (queryStoreState.queryPoints.filter(point => point.isInitialized).length > 0) {
             queryStoreState.queryPoints
@@ -46,10 +40,8 @@ export default class NavBar {
                 .forEach(pointAsString => result.searchParams.append('point', pointAsString))
         }
 
-        if (!first || queryStoreState.routingProfile.name != Object.keys(config.profiles ?? {})[0])
-            result.searchParams.append('profile', queryStoreState.routingProfile.name)
-        if (!first || mapState.selectedStyle.name != config.defaultTiles)
-            result.searchParams.append('layer', mapState.selectedStyle.name)
+        result.searchParams.append('profile', queryStoreState.routingProfile.name)
+        result.searchParams.append('layer', mapState.selectedStyle.name)
         if (queryStoreState.customModelEnabled)
             result.searchParams.append('custom_model', queryStoreState.customModelStr.replace(/\s+/g, ''))
 
@@ -166,16 +158,15 @@ export default class NavBar {
 
     public updateUrlFromState() {
         if (this.ignoreStateUpdates) return
-        const newHref = this.createUrlFromState(false)
+        const newHref = this.createUrlFromState()
         if (newHref !== window.location.href) window.history.pushState(null, '', newHref)
     }
 
-    private createUrlFromState(first: boolean) {
+    private createUrlFromState() {
         return NavBar.createUrl(
             window.location.origin + window.location.pathname,
             this.queryStore.state,
-            this.mapStore.state,
-            first
+            this.mapStore.state
         ).toString()
     }
 
