3d0514865cff02a2111cb34a8b7501ff2e9be00c
Peter <graphhopper@gmx.de>
2024-07-06 10:59:53 +0200

    disable zoom for all actions in context menu

--- src/map/ContextMenuContent.tsx
+++ src/map/ContextMenuContent.tsx
@@ -35,7 +35,7 @@ export function ContextMenuContent({
                     queryText: coordinateToText(coordinate),
                     isInitialized: true,
                 },
-                true
+                false
             )
         )
     }
@@ -62,7 +62,7 @@ export function ContextMenuContent({
             // to be closest to the clicked location, because for every route the n-th snapped_waypoint corresponds to
             // the n-th query point
             const index = findNextWayPoint(routes, coordinate).nextWayPoint
-            Dispatcher.dispatch(new AddPoint(index, coordinate, true, true))
+            Dispatcher.dispatch(new AddPoint(index, coordinate, true, false))
         }
     }
 
