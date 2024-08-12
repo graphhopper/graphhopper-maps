2670bcd903c846f7efcbc38980e35a9cc4c040ac
Peter <graphhopper@gmx.de>
2024-08-12 16:47:25 +0200

    i18n: updated and 'mn' added

--- update-translations.py
+++ update-translations.py
@@ -3,7 +3,7 @@ import io
 import requests
 
 destination = "./src/translation/tr.json"
-translations = "en_US SKIP SKIP ar ast az bg bn_BN ca cs_CZ da_DK de_DE el eo es fa fil fi fr_FR fr_CH gl he hr_HR hsb hu_HU in_ID it ja ko kz lt_LT nb_NO ne nl pl_PL pt_BR pt_PT ro ru sk sl_SI sr_RS sv_SE tr uk uz vi_VN zh_CN zh_HK zh_TW"
+translations = "en_US SKIP SKIP ar ast az bg bn_BN ca cs_CZ da_DK de_DE el eo es fa fil fi fr_FR fr_CH gl he hr_HR hsb hu_HU in_ID it ja ko kz lt_LT mn nb_NO ne nl pl_PL pt_BR pt_PT ro ru sk sl_SI sr_RS sv_SE tr uk uz vi_VN zh_CN zh_HK zh_TW"
 
 r = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vTjOxfOBVw9VvEroPw30w77XA-JCCbraf4GeL9URMgK0kjfS-YT5R8TT6PACF8O7o6fhPKMsWKFf9M-/pub?output=tsv")
 r.encoding = 'utf-8'  # useful if encoding is not sent (or not sent properly) by the server
