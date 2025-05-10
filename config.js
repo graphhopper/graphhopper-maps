090b25777046bdc701464de568935e90e917c750
Peter <graphhopper@gmx.de>
2025-05-10 03:14:10 +0200

    config change

--- config.js
+++ config.js
@@ -2,73 +2,80 @@
  * Webpack will replace this file with config-local.js if it exists
  */
 const config = {
-    // the url of the GraphHopper routing backend, either use graphhopper.com or point it to your own GH instance
-    routingApi: 'https://graphhopper.com/api/1/',
-    // the url of the geocoding backend, either use graphhopper.com or point it to another geocoding service. use an empty string to disable the address search
-    geocodingApi: 'https://graphhopper.com/api/1/',
-    // the tile layer used by default, see MapOptionsStore.ts for all options
-    defaultTiles: 'OpenStreetMap',
-    // various api keys used for the GH backend and the different tile providers
+   // routingApi: 'https://graphhopper.com/api/1/',
+    routingApi: 'http://localhost:8989/',
+   // geocodingApi: 'https://graphhopper.com/api/1/',
+    geocodingApi: 'http://localhost:8080/',
+    defaultTiles: 'Planetiler',
+    routingGraphLayerAllowed: true,
     keys: {
-        graphhopper: 'efc33bcc-a9e6-450b-9221-c52c5bf57de3',
-        maptiler: 'missing_api_key',
-        omniscale: 'missing_api_key',
-        thunderforest: 'missing_api_key',
-        kurviger: 'missing_api_key',
-        tracestrack: 'missing_api_key',
+        graphhopper: 'no_api_key',
+        maptiler: 'no_api_key',
+        omniscale: 'no_api_key',
+        thunderforest: 'no_api_key',
+        kurviger: 'no_api_key',
+        tracestrack: 'no_api_key'
     },
-    // if true there will be an option to enable the GraphHopper routing graph and the urban density visualization in the layers menu
-    routingGraphLayerAllowed: false,
-    // parameters used for the routing request generation
+    externalMVTLayer: {
+        // metric: dist,time or traffic_time -> specifies by which metric the GH routes shall be compared to the benchmark
+        // alternative: -1, 0, 1, ... -> specifies which alternative route shall be taken from the benchmark. 0 is the 'best' route. -1 means we use the one that is closest to GH with respect to metric
+        // profile: the GH profile we use for the comparison
+        // min_error: for example a value of 10 means that only benchmarks with relative error >10% (regarding metric,profile+alternative)
+        // only_if_first_two_profiles_different: if true only benchmarks are drawn on the map for which the GH results (regarding metric) for the first two profiles differ (by more than 1%)
+        url: 'http://135.181.79.179:8900/benchmarks-mvt/{z}/{x}/{y}.mvt?metric=dist&alternative=-1&profile=car&min_error=0&only_if_first_two_profiles_different=true',
+        styles: {
+            // use this
+            'benchmark': {
+                color: [255, 0, 0, 0.8], // red
+                width: 3
+            },
+            'gh_car': {
+                color: [0, 255, 0, 0.8], // green
+                width: 3
+            },
+            'gh_car_old_ud_ee': {
+                color: [0, 0, 255, 0.8], // blue
+                width: 3
+            },
+        }
+    },
     request: {
-        details: [
-            'road_class',
-            'road_environment',
-            'road_access',
-            'surface',
-            'max_speed',
-            'average_speed',
-            'toll',
-            'track_type',
-            'country',
-        ],
+        details: ["road_class","road_environment","road_access","surface","max_speed","average_speed","toll","track_type","country"],
+        snapPreventions: []
     },
-
-    // Use 'profiles' to define which profiles are visible and how. Useful if the /info endpoint contains too many or too "ugly" profile
-    // names or in the wrong order. The key of each profile will be used as name and the given fields will overwrite the fields of the
-    // default routing request. The following example is tuned towards the GraphHopper Directions API. If you have an own server you might want to adapt it.
-    //
-    // profiles: {
-    //    car:{}, small_truck:{}, truck:{}, scooter:{},
-    //    foot:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating'] }, hike:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating' ] },
-    //    bike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating' ] }, mtb:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] }, racingbike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
-    // }
-    //
-    // E.g. the 'bike' entry will add a "bike" profile for which we send a request with the specified 'details' parameter. You can even change the profile itself when you specify
-    // bike: { profile: 'raw_bike', ... }
-
-    // You can 'collapse' or group certain profiles to reduce the number of profiles in the panel. Instead they're listed in the settings but still a profile icon is shown.
-    // Note: the name of the group must be the default option for this group.
     profile_group_mapping: {},
-    // profile_group_mapping: {
-    //  car: {
-    //    options: [
-    //      { profile: 'car' },
-    //      { profile: 'car_avoid_motorway' },
-    //      { profile: 'car_avoid_ferry' },
-    //      { profile: 'car_avoid_toll' }
-    //    ]
-    //  },
-    //  bike: {
-    //    options: [
-    //      { profile: 'bike' },
-    //      { profile: 'mtb' },
-    //      { profile: 'racingbike' },
-    //      { profile: 'ecargobike' }
-    //    ]
-    //  }
-    // }
+    //profile_group_mapping: {
+    //   car: {
+    //     options: [
+    //       { profile: 'car',  },
+    //       { profile: 'car_avoid_motorway' },
+    //       { profile: 'car_avoid_ferry' },
+    //       { profile: 'car_avoid_toll' }
+    //     ]
+    //   },
+    //},
+    //   racingbike: {
+    //     options: [
+    //       { profile: 'racingbike' },
+    //       { profile: 'ecargobike' }
+    //     ]
+    //   },
+    //   truck: {
+    //     options: [
+    //       { profile: 'small_truck' },
+    //       { profile: 'truck' }
+    //     ]
+    //   },
+    //   foot: {
+    //     options: [
+    //       { profile: 'foot' },
+    //       { profile: 'hike' }
+    //     ]
+    //   }
+    // },
+    profiles: {
+      car:{},
+      foot:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating'] }, 
+      bike:{ details: ['get_off_bike', 'access_conditional', 'bike_network', 'bike_conditional' ] }, 
+    }
 }
-
-// this is needed for jest (with our current setup at least)
-if (module) module.exports = config
