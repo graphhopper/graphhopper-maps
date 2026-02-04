c6ce091c82493afcfd374095b1f683559a4cf0b4
Peter <graphhopper@gmx.de>
2026-02-04 18:03:47 +0100

    revert config.js

--- config.js
+++ config.js
@@ -1,18 +1,21 @@
+/**
+ * Webpack will replace this file with config-local.js if it exists
+ */
 const config = {
-    // the url of the GraphHopper backend, either use graphhopper.com or point it to your own GH instance
-    api: 'https://graphhopper.com/api/1/',
-    // the tile layer used by default, see MapOptionsStore.ts for all options
+    // the url of the GraphHopper routing backend, either use graphhopper.com or point it to your own GH instance
     routingApi: 'https://graphhopper.com/api/1/',
+    // the url of the geocoding backend, either use graphhopper.com or point it to another geocoding service. use an empty string to disable the address search
     geocodingApi: 'https://graphhopper.com/api/1/',
+    // the tile layer used by default, see MapOptionsStore.ts for all options
     defaultTiles: 'OpenStreetMap',
-    navigationTiles: 'Mapilion',
     // various api keys used for the GH backend and the different tile providers
     keys: {
-     "graphhopper":"e96f6517-5a46-4e3d-9773-4956d7b6046f",
-     "maptiler":"_undefined_",
-     "omniscale":"_undefined_",
-     "thunderforest":"4f283c77a8644d0b8ead1a3c3faf04ba",
-     "kurviger":"b582abd4-d55d-4cb1-8f34-f4254cd52aa7"
+        graphhopper: 'efc33bcc-a9e6-450b-9221-c52c5bf57de3',
+        maptiler: 'missing_api_key',
+        omniscale: 'missing_api_key',
+        thunderforest: 'missing_api_key',
+        kurviger: 'missing_api_key',
+        tracestrack: 'missing_api_key',
     },
     // if true there will be an option to enable the GraphHopper routing graph and the urban density visualization in the layers menu
     routingGraphLayerAllowed: false,
@@ -29,45 +32,43 @@ const config = {
             'track_type',
             'country',
         ],
-        snapPreventions: ['ferry'],
     },
-    profile_group_mapping: {
-      car: {
-        options: [
-          { profile: 'car' },
-          { profile: 'car_avoid_motorway' },
-          { profile: 'car_avoid_ferry' },
-          { profile: 'car_avoid_toll' },
-        ]
-      },
-      racingbike: {
-        options: [
-          { profile: 'racingbike' },
-          { profile: 'ecargobike' } 
-        ]
-      },
-      truck: {
-        options: [
-          { profile: 'small_truck' },
-          { profile: 'truck' },
-        ]
-      },
-      foot: {
-        options: [
-          { profile: 'foot' },
-          { profile: 'hike' },
-        ]
-      },
-    },
-    profiles: {
-      car:{}, car_avoid_motorway:{}, car_avoid_toll:{}, car_avoid_ferry:{},
-      small_truck:{}, truck:{},
-      scooter:{},
-      foot:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating'] },
-      hike:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating' ] },
-      bike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating' ] },
-      mtb:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
-      racingbike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
-      ecargobike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
-    },
+
+    // Use 'profiles' to define which profiles are visible and how. Useful if the /info endpoint contains too many or too "ugly" profile
+    // names or in the wrong order. The key of each profile will be used as name and the given fields will overwrite the fields of the
+    // default routing request. The following example is tuned towards the GraphHopper Directions API. If you have an own server you might want to adapt it.
+    //
+    // profiles: {
+    //    car:{}, small_truck:{}, truck:{}, scooter:{},
+    //    foot:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating'] }, hike:{ details: ['foot_network', 'access_conditional', 'foot_conditional', 'hike_rating' ] },
+    //    bike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating' ] }, mtb:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] }, racingbike:{ details: ['get_off_bike', 'bike_network', 'access_conditional', 'bike_conditional', 'mtb_rating'] },
+    // }
+    //
+    // E.g. the 'bike' entry will add a "bike" profile for which we send a request with the specified 'details' parameter. You can even change the profile itself when you specify
+    // bike: { profile: 'raw_bike', ... }
+
+    // You can 'collapse' or group certain profiles to reduce the number of profiles in the panel. Instead they're listed in the settings but still a profile icon is shown.
+    // Note: the name of the group must be the default option for this group.
+    profile_group_mapping: {},
+    // profile_group_mapping: {
+    //  car: {
+    //    options: [
+    //      { profile: 'car' },
+    //      { profile: 'car_avoid_motorway' },
+    //      { profile: 'car_avoid_ferry' },
+    //      { profile: 'car_avoid_toll' }
+    //    ]
+    //  },
+    //  bike: {
+    //    options: [
+    //      { profile: 'bike' },
+    //      { profile: 'mtb' },
+    //      { profile: 'racingbike' },
+    //      { profile: 'ecargobike' }
+    //    ]
+    //  }
+    // }
 }
+
+// this is needed for jest (with our current setup at least)
+if (module) module.exports = config
