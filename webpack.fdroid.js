2c4940139e4dfa40c0a47471d0d22f45cbe2b666
Peter <graphhopper@gmx.de>
2026-04-14 11:17:59 +0200

    avoid favicons for fdroid build

--- webpack.fdroid.js
+++ webpack.fdroid.js
@@ -3,4 +3,5 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: common.plugins?.filter(p => p.constructor.name !== 'FaviconsWebpackPlugin') || [],
 })
