e269860d5c6a021ea789203e20bf95a20492014b
Peter <graphhopper@gmx.de>
2026-08-15 16:44:48 +0200

    lazy loading maplibre needs to be avoided for fdroid (to produce a single bundle)

--- webpack.fdroid.js
+++ webpack.fdroid.js
@@ -3,5 +3,8 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
+    // The Android app bundles everything offline and expects a single bundle.js, so disable
+    // code splitting (e.g. the lazily imported MapLibreLayer) for this build only.
+    output: { asyncChunks: false },
     plugins: common.plugins?.filter(p => p.constructor.name !== 'FaviconsWebpackPlugin') || [],
 })
