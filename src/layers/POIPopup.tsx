b1e22226259befa68b73b38458bfa22b6f9a054d
Peter <graphhopper@gmx.de>
2026-09-25 00:12:27 +0200

    handover street info from poi

--- src/layers/POIPopup.tsx
+++ src/layers/POIPopup.tsx
@@ -76,7 +76,7 @@ export default function POIStatePopup({ map, poiState, points }: POIStatePopupPr
             const queryPoint = {
                 ...points[index],
                 queryText: selectedPOI?.name,
-                streetName: '',
+                streetName: selectedPOI?.tags['addr:street'] ?? '',
                 coordinate: selectedPOI?.coordinate,
                 isInitialized: true,
             }
